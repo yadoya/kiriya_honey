41c034c8dc80a78613898bd24aed531e0c802109
yadoya <miyabayashi88@gmail.com>
2014-05-15 00:44:05 +0900

    add zero? empty?

--- lib/kiriya_honey/extention/object.rb
+++ lib/kiriya_honey/extention/object.rb
@@ -1,7 +1,15 @@
 # -*- coding: utf-8 -*-
 class Object
   def odayaka?
-    return "この穏やかさ 穏やかじゃないわね。" if self.nil?
-    return "穏やかじゃないわね"
+    str_odayakajanai = "穏やかじゃないわね"
+    str_odayaka_off = "この穏やかさ 穏やかじゃないわね。"
+
+    return str_odayaka_off if self.is_a? String and self.empty?
+    return str_odayaka_off if self.is_a? Array and self.empty?
+    return str_odayaka_off if self.is_a? Hash and self.empty?
+    return str_odayaka_off if self.is_a? Numeric and self.zero?
+    return str_odayaka_off if self.nil?
+    str_odayakajanai
   end
+
 end
--- spec/extention/object_spec.rb
+++ spec/extention/object_spec.rb
@@ -1,7 +1,19 @@
 # -*- encoding: UTF-8 -*-
 require File.expand_path(File.join('../', 'spec_helper'), File.dirname(__FILE__))
 
+
+str_odayakajanai = "穏やかじゃないわね"
+str_odayaka_off = "この穏やかさ 穏やかじゃないわね。"
+
 describe Object do
- it "Ichigo should odayakajanai"    do "Ichigo".odayaka?.should == "穏やかじゃないわね"; end
- it "nil should yappari odayakajanai"  do nil.odayaka?.should == "この穏やかさ 穏やかじゃないわね。" end
+ it "Ichigo should odayakajanai"    do "Ichigo".odayaka?.should == str_odayakajanai; end
+ it "blank should yappari odayakajanai"    do "".odayaka?.should == str_odayaka_off; end
+ it "Soleil Array should odayakajanai"    do ["Ichigo","Aoi","Ran"].odayaka?.should == str_odayakajanai; end
+ it "blank Array should yappari odayakajanai"    do [].odayaka?.should == str_odayaka_off; end
+ it "Soleil Hash should odayakajanai"    do {Ichigo: "Osyamoji",Aoi: "Ichigo",Ran: "Wakame"}.odayaka?.should == str_odayakajanai; end
+ it "blank Hash should yappari odayakajanai"    do {}.odayaka?.should == str_odayaka_off; end
+ it "1 should odayakajanai"    do 1.odayaka?.should == str_odayakajanai; end
+ it "0 should yappari odayakajanai"    do 0.odayaka?.should == str_odayaka_off; end
+ it "nil should yappari odayakajanai"  do nil.odayaka?.should == str_odayaka_off end
+
 end
